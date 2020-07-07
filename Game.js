5b9bee677720038f2f78f1a48887d8ec00a78c34
samir Saad <samirsaad_ca@yahoo.com>
2020-07-07 00:04:35 -0400

    add wiki

--- Game.js
+++ Game.js
@@ -31,7 +31,7 @@ module.exports = class Game{
         let sReply = "";
         switch(this.stateCur){
             case GameState.WELCOMING:
-                sReply = "Hi, you have 3 clues for each  question ,"+
+                sReply = "Hi, you have 3 clues for each question ,"+
                 " after each incorrect answer type go,"+
                 "let's have some fun!!" +"How about I SPY !!, type Go";
                 this.stateCur = GameState.PLAY;
